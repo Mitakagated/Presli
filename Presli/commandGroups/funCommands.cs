67b5e481710a0bce21f10f3af1e6c9fc1504749f
MitakaGated <dimitarpetrov44@gmail.com>
2023-02-25 15:00:57 +0200

    Added random 9 nums command

--- Presli/commandGroups/funCommands.cs
+++ Presli/commandGroups/funCommands.cs
@@ -14,4 +14,25 @@ public class funCommands : ApplicationCommandModule
             new DiscordInteractionResponseBuilder()
                 .WithContent("fuck you"));
     }
+
+    [SlashCommand("random_9_cifri", "generirane na 9 chisla ot 0 do 9")]
+    public async Task randomNum(InteractionContext ctx)
+    {
+        var num = new Random();
+        int[] nums = new int[9];
+        var response = "";
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] = num.Next(0, 10);
+        }
+
+        foreach (var number in nums)
+        {
+            response += number + " ";
+        }
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder()
+                .WithContent(response));
+    }
 }
